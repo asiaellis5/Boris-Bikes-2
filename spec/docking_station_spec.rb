e3758200fc463590172ee9b8d1f6a79a31fa02de
Asia <asiaellis5@gmail.com>
2020-04-16 13:51:24 +0100

    release bike releases a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,15 @@ require 'docking_station'
 
 describe DockingStation do
   let(:subject) { described_class.new }
+  let(:bike){ double(:bike) }
 
   describe '#release_bike' do
     it 'docking station responds to the method release_bike' do
       expect(subject).to respond_to :release_bike
+    end
+
+    it "releases a bike" do
+      expect(subject.release_bike(bike)).to eq bike
     end
   end
 end
