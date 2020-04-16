befd299924500bd66538d2e1e4c55b102e7c7392
Asia <asiaellis5@gmail.com>
2020-04-16 15:21:42 +0100

    refactored to separate concerns

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,7 +4,7 @@ require_relative 'bike'
 
 class DockingStation
 
-  DEFAULT_CAPACITY = 10
+  DEFAULT_CAPACITY = 20
 
   attr_reader :bike, :docked_bikes
 
@@ -13,12 +13,22 @@ class DockingStation
   end
 
   def release_bike
-    raise 'No bikes available' if @docked_bikes.length <= 0
+    raise 'No bikes available' if empty?
     @docked_bikes.pop
   end
 
   def dock_bike(bike)
-    raise 'Docking Station Full' if @docked_bikes.length >= DEFAULT_CAPACITY
+    raise 'Docking Station Full' if full?
     @docked_bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    @docked_bikes.length >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @docked_bikes.length <= 0
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -39,7 +39,7 @@ describe DockingStation do
     end
 
     it "raises an error if dock is full" do
-      10.times { subject.dock_bike(bike) }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(bike) }
       expect{subject.dock_bike(bike)}.to raise_error 'Docking Station Full'
     end
   end
