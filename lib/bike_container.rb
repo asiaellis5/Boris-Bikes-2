911579545525782b1d210d7a1b7fd83d54a65fd8
Asia <asiaellis5@gmail.com>
2020-04-22 12:16:23 +0100

    added the add_bike functionality to the module

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -3,8 +3,26 @@ module BikeContainer
 
   attr_reader :capacity
 
-  def initialize
-    @capacity = DEFAULT_CAPACITY
+  def initialize(capacity= DEFAULT_CAPACITY)
+    @capacity = capacity
+    @bikes = []
   end
+
+  def add_bike(bike)
+    raise "#{self.class.name} full" if full?
+    @bikes.push(bike)
+  end
+
+  def full?
+    @bikes.length >= @capacity
+  end
+
+  def empty?
+    @bikes.length <= 0
+  end
+
+  private
+
+  attr_reader :bike
 
 end
